d62733942519a15ed2c5fa29255167ae4c801edf
Ilia <120221293+striction10@users.noreply.github.com>
2023-09-17 00:51:15 +0700

    Update main.cpp, add header, add file_info_function.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <clocale>
-#include <sstream>
+#include "file_info_structs.h"
 
-using namespace std;
-
-struct FileInfo{
-    string name_file;
-    string data;
-    uintmax_t file_size;
-};
-
-
-int main(){
+int main() {
     setlocale(LC_ALL, "");
-    fstream in("test.txt");
-
-    if (!in.is_open()){
-        cout << "Не удалось открыть файл" << endl;
-        return 0;
-    }
-    vector<FileInfo> file_info;
-    string line;
-    while(getline(in, line)){
-        FileInfo file;
-        istringstream inf(line);
-        inf >> file.name_file >> file.data >> file.file_size;
-        file_info.push_back(file);
-    }
-    for (const FileInfo& file : file_info){
-        cout << "\nНазвание файла: " << file.name_file;
-        cout << "\nДата: " << file.data;
-        cout << "\nРазмер: " << file.file_size;
-        cout << "\n";
-    }
-
-    in.close();
+    string file_name = "test.txt";
+    vector<FileInfo> file_info = readData(file_name);
+    printFileInfo(file_info);
     return 0;
 }
