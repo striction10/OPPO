4db2f9eea77b0700ef035412a6e33a6455e769b2
Ilia <120221293+striction10@users.noreply.github.com>
2023-09-28 15:47:25 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include "file_info_structs.h"
+#include "FileUntils.hpp"
 
 int main() {
     setlocale(LC_ALL, "");
     string file_name = "test.txt";
-    vector<FileInfo> file_info = readData(file_name);
+    ifstream input(file_name);
+    if (!input.is_open()) {
+        cout << "Не удалось открыть файл" << endl;
+    }
+    vector<FileInfo> file_info = readData(input);
     printFileInfo(file_info);
     return 0;
 }
