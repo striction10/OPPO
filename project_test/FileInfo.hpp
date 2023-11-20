c22fd4c2b789fa19c6c8a98b80a1b97978c3d4c2
Ilia <120221293+striction10@users.noreply.github.com>
2023-11-20 17:46:09 +0700

    Update FileInfo.hpp

--- project_test/FileInfo.hpp
+++ project_test/FileInfo.hpp
@@ -9,17 +9,21 @@
 using namespace std;
 
 class FileInfo{
-public:
+private:
     string name_file;
     Date date;
     int file_size;
-
-    void readNameFile(istringstream& file);
-    void readDate(istringstream& file);
-    void readFileSize(istringstream& file);
+public:
+    vector<string> getArgs(string line);
+    void readNameFile(string line);
+    void readDate(string line);
+    void readFileSize(string line);
     void readFileInfoLine(const string& line);
-    void printLayoutFileInfo() const;
+    void printLayoutFileInfo(std::ostream& out) const;
+    string get_name_file();
+    Date get_date();
+    int get_file_size();
 };
 
-vector<FileInfo> readData(ifstream& input);
+vector<FileInfo> readData(istream& input);
 void printFileInfo(const vector<FileInfo>& file_info);
